1237c159e06436d1afa518bc6622084c5698d081
Gary Von Schilling <garyv@juicyorange.com>
2011-09-23 22:14:41 -0400

    Added config options to kittify function

--- kittify.js
+++ kittify.js
@@ -1,20 +1,43 @@
 /*
- *  Kittify Plugin
- *  
- *  Copyright 2011 Gary Von Schilling
- *  Project on github: https://github.com/garyv/kittify
+ * Kittify 2.0.0
  *
- *  How to use:
- *  kittify(); 
- *  
+ * Copyright 2011, Gary Von Schilling
+ * Project: https://github.com/garyv/kittify
+ *
+ * How to use:
+ *
+ * Replace all images with kittens
+ * kittify(); 
+ *
+ * Configure with an options object
+ * kittify(options); 
+ *
+ * Set a minimum image size 
+ * kittify({size:100});
+ *
+ * Replace images in a jQuery object or an array of images
+ * kittify({imgs:$('img:first')});
+ * kittify({imgs:document.getElementById('page').getElementsByTagName('img')});
+ * 
+ * Set to a different image generator
+ * kittify({template:'http://placehold.it/-w-x-h-'});
+ * kittify({template:'http://lorempixum.com/-w-/-h-/animals'});
+ * kittify({template:'http://lorempixum.com/-w-/-h-/food'});
+ * kittify({template:'http://lorempixum.com/-w-/-h-/people'});
+ * kittify({template:'http://lorempixum.com/-w-/-h-/technics'});
+ * kittify({template:'http://dummyimage.com/-w-/-h-/09f/fff.png'});
+ * 
  */
 
-function kittify() {
+function kittify(options) {
 
-    var i,
-        t = 'http://placekitten.com/-w-/-h-',
-        imgs = document.getElementsByTagName('img');
+    options = options || {};
     
+    var imgs = options.imgs || document.getElementsByTagName('img'),
+        size = options.size || 20,
+        template = options.template || 'http://placekitten.com/-w-/-h-',
+        i;
+        
     for (i=0; i < imgs.length; i += 1) {
         
         (function(){
@@ -25,8 +48,8 @@ function kittify() {
                  h  = img.height || img.offsetHeight ||
                        Math.floor(img.style.height || 0);
             
-            if (w > 20 && h > 20) {
-                img.src = t.replace(/-w-/g, w).replace(/-h-/g, h);
+            if (w > size && h > size) {
+                img.src = template.replace(/-w-/g, w).replace(/-h-/g, h);
             }
     
         })();
