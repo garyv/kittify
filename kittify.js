5e70f675bf651eb269c00f35dcfbb51ed74c8da3
Gary Von Schilling <garyv@juicyorange.com>
2012-02-02 21:51:16 -0500

    stop repeating the same images when they are all the same size

--- kittify.js
+++ kittify.js
@@ -1,7 +1,7 @@
 /*
- * Kittify 2.0.0
+ * Kittify 3.0.0
  *
- * Copyright 2011, Gary Von Schilling
+ * Copyright 2012, Gary Von Schilling
  * Project: https://github.com/garyv/kittify
  *
  * How to use:
@@ -15,6 +15,9 @@
  * Set a minimum image size 
  * kittify({size:100});
  *
+ * Allow kitten images to be repeated
+ * kittify({repeat:true});
+ *
  * Replace images in a jQuery object or an array of images
  * kittify({imgs:$('img:first')});
  * kittify({imgs:document.getElementById('page').getElementsByTagName('img')});
@@ -26,7 +29,7 @@
  * kittify({template:'http://lorempixum.com/-w-/-h-/people'});
  * kittify({template:'http://lorempixum.com/-w-/-h-/technics'});
  * kittify({template:'http://dummyimage.com/-w-/-h-/09f/fff.png'});
- * 
+ *
  */
 
 function kittify(options) {
@@ -36,6 +39,9 @@ function kittify(options) {
     var imgs = options.imgs || document.getElementsByTagName('img'),
         size = options.size || 20,
         template = options.template || 'http://placekitten.com/-w-/-h-',
+        repeat = options.repeat || false,
+        alternate = false,
+        sizes = {},
         i;
         
     for (i=0; i < imgs.length; i += 1) {
@@ -46,9 +52,23 @@ function kittify(options) {
                  w  = img.width || img.offsetWidth ||
                        Math.floor(img.style.width || 0),
                  h  = img.height || img.offsetHeight ||
-                       Math.floor(img.style.height || 0);
-            
+                       Math.floor(img.style.height || 0)
+                 key = w + 'x' + h;
+                 
             if (w > size && h > size) {
+                
+                while(sizes[key] && !repeat) {
+                    if (alternate) {
+                        w -= 1;
+                    } else {
+                        h -= 1;
+                    }
+                    key = w + 'x' + h;
+                    alternate = !alternate;
+                }
+                
+                sizes[key] = true;
+            
                 img.src = template.replace(/-w-/g, w).replace(/-h-/g, h);
             }
     
